4f38cd6b60bffe3f78506d5ff7dbd1af97d7b103
Derek Leung <dltnf.alt@gmail.com>
2012-11-12 10:54:09 -0800

    removed DOS support, modified read loop to last until EOF

--- pathnorm.py
+++ pathnorm.py
@@ -1,30 +1,5 @@
 # Using python version 3.3
 
-from re import compile, match
-
-UNIX = 0
-DOS = 1
-SEPARATORS = {UNIX: '/', DOS: '\\'}
-DOS_PREFIX_PATTERN = compile('.*:')
-
-def _isabs(path, system=UNIX):
-    '''Determines whether the given pathname is an absolute path on the given
-    system.
-
-    If the given pathname is a relative path, this only returns False.
-    Otherwise, it returns a tuple: one with the absolute path prefix, and one
-    with the rest of the path.
-    '''
-    separator = SEPARATORS[system]
-    if system == UNIX:
-        if path[0] == separator:
-            return (separator, path[1:])
-    elif system == DOS:
-        prefix = path.split(separator)[0]
-        if DOS_PREFIX_PATTERN.match(prefix):
-            return (prefix + separator, path[len(prefix) + 1:])
-    return False
-
 def _build_path_list(path, separator='/'):
     '''Returns a list containing the structure of directories represented by
     a relative path.'''
@@ -45,23 +20,21 @@ def _build_path_list(path, separator='/'):
 
     return structure
 
-def normalize(path, system=UNIX):
+def normalize(path):
     '''Returns a normalized filepath string.'''
     if len(path) == 0:
         return path
-    
-    absolute = _isabs(path, system)
-    if absolute:
-        prefix, path = absolute
-    else:
-        prefix = ''
-    
-    separator = SEPARATORS[system]
-    dirs = _build_path_list(path, separator) # holds directories on the path
+    if path[0] == '/': # this path is absolute
+        prefix = '/'
+        path = path[1:]
+    else: # this path is relative
+        prefix = '' 
+        
+    dirs = _build_path_list(path, '/') # holds directories on the path
     if dirs == None:
         return path
     else:
-        return prefix + str.join(separator, dirs)
+        return prefix + str.join('/', dirs)
 
 def _test():
     '''Run a test suite for this module.'''
@@ -85,8 +58,13 @@ def _test():
     print('All tests have passed.')
 
 if __name__ == '__main__':
-    _test()
-    path = input()
-    while path != '':
-        print(normalize(path, UNIX))
-        path = input()
+    # poll for input until EOF and apply normalize
+    try:
+        line = input()
+        while True:
+            paths = line.split(' ')
+            for path in paths:
+                print(normalize(path))
+            line = input()
+    except EOFError:
+        pass
